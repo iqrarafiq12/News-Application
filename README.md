ae858c6d4dc354242fcf4f301d933aa7d5103c9c
Iqra Rafiq <123827193+iqrarafiq12@users.noreply.github.com>
2023-03-09 00:22:49 +0500

    Merge branch 'master' into pr/2

--- README.md
+++ README.md
@@ -6,3 +6,9 @@ News Api and fake api
 
 In javaScript file you can see that in the beginning you can see i first use fake api 
 After i use news api you can see both of the api output in the console.
+
+firstly i working the main tag / section of main page and some news or updates are avilible of the main news sections.
+
+window scroll navigation bar setting on navbar through the javascript.
+
+i make a some pictures folders for the data of images .
--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+let nav = document.getElementById("scroll-wrap");
+let sticky = nav.offsetTop;
+window.onscroll = function() {sticker()};
+function sticker() {
+   if (window.pageYOffset >= sticky) {
+      nav.classList.add("sticky")
+   } else {
+      nav.classList.remove("sticky");
+   }
+}
 // Let's fetch some data and add it into the console:
 
 let fetchData = fetch("https://newsdata.io/api/1/news?apikey=pub_18331403a2251a60b2cda22c5b6c24bab0586&q=cricket")
