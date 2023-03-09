c96baf3ff35e078df13c5e3bedb8b6f429d85d41
Fardeen Awais <fardeenjutt99@gmail.com>
2023-03-09 21:01:00 +0500

    Adding 100 news post in the home

--- script.js
+++ script.js
@@ -1,18 +1,54 @@
 let nav = document.getElementById("scroll-wrap");
 let sticky = nav.offsetTop;
-window.onscroll = function() {sticker()};
+window.onscroll = function () {
+  sticker();
+};
 function sticker() {
-   if (window.pageYOffset >= sticky) {
-      nav.classList.add("sticky")
-   } else {
-      nav.classList.remove("sticky");
-   }
+  if (window.pageYOffset >= sticky) {
+    nav.classList.add("sticky");
+  } else {
+    nav.classList.remove("sticky");
+  }
 }
 // Let's fetch some data and add it into the console:
 
-let fetchData = fetch("https://newsdata.io/api/1/news?apikey=pub_18331403a2251a60b2cda22c5b6c24bab0586&q=cricket")
-fetchData.then((value1)=>{
-  return value1.json()
-}).then((value2)=>{
-  console.log({value2}) //TODO: Check the console you can see an Object we will add it Later on our News App
-})
+async function fetchNewsData() {
+  try {
+    const response = await fetch(
+      "https://newsapi.org/v2/everything?q=tesla&from=2023-02-09&sortBy=publishedAt&apiKey=5972328e0ace46a6b88a38732dc4529b"
+    );
+
+    const responseData = await response.json();
+    console.log(responseData);
+
+    const newsData = responseData.articles;
+    const newsList = document.getElementById("ul");
+
+    newsData.forEach((article) => {
+      const newstitle = document.createElement("h2");
+      const newsdesc = document.createElement("p");
+      const newsauthor = document.createElement("p");
+
+      newstitle.textContent = article.title;
+      newsdesc.textContent = article.description;
+      newsauthor.textContent = article.author;
+
+      // Adding Styles
+      newstitle.classList.add("title");
+      newsdesc.classList.add("desc");
+      newsauthor.classList.add("author");
+
+      const listItem = document.createElement("li");
+      listItem.appendChild(newstitle);
+      listItem.appendChild(newsdesc);
+      listItem.appendChild(newsauthor);
+      newsList.appendChild(listItem);
+    });
+
+    document.getElementById("news").appendChild(newsList);
+  } catch (error) {
+    console.log(`Oh bhai check kr: ${error}`);
+  }
+}
+
+fetchNewsData();
